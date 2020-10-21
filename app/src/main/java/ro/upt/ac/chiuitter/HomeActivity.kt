1a0fe7076473e3cfd26c51101b70b7eeebf182e5
Luigi Bolovan <luigibolovan@gmail.com>
2020-10-21 22:20:46 +0300

    todos done week 4

--- app/src/main/java/ro/upt/ac/chiuitter/HomeActivity.kt
+++ app/src/main/java/ro/upt/ac/chiuitter/HomeActivity.kt
@@ -3,7 +3,13 @@ package ro.upt.ac.chiuitter
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_chiuit.*
+import kotlinx.android.synthetic.main.view_home.*
+import ro.upt.ac.chiuitter.ComposeActivity.Companion.EXTRA_TEXT
 
 class HomeActivity : AppCompatActivity() {
 
@@ -15,13 +21,19 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.view_home)
 
+        fab_add.setOnClickListener(View.OnClickListener { composeChiuit() })
+
         initList()
     }
 
     private fun initList() {
         val chiuitList = dummyChiuitStore.getAllData()
 
-        TODO("7. Instantiate the adapter, then setup the recycler view list")
+        listAdapter = ChiuitRecyclerViewAdapter(chiuitList.toMutableList()){
+            shareChiuit(it.description)
+        }
+        rv_chiuit_list.adapter = listAdapter
+        rv_chiuit_list.layoutManager = LinearLayoutManager(this)
     }
 
     /*
@@ -30,8 +42,11 @@ class HomeActivity : AppCompatActivity() {
      */
     private fun shareChiuit(text: String) {
         val sendIntent = Intent().apply {
-            // TODO 1: Configure to support text sending/sharing and then attach the text as intent's extra.
-
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, text)
+            }
 
         }
 
@@ -41,15 +56,13 @@ class HomeActivity : AppCompatActivity() {
     }
 
     /*
-    Defines an *explicit* intent which will be used to start ComposeActivity.
+     * Defines an *explicit* intent which will be used to start ComposeActivity.
      */
     private fun composeChiuit() {
-        // TODO 2: Create an explicit intent which points to ComposeActivity.
+        val composeActivityIntent = Intent(this, ComposeActivity::class.java)
 
-
-        // TODO 3: Start a new activity with the previously defined intent.
         // We start a new activity that we expect to return the acquired text as the result.
-
+        startActivityForResult(composeActivityIntent, COMPOSE_REQUEST_CODE)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -61,13 +74,11 @@ class HomeActivity : AppCompatActivity() {
 
     private fun extractText(data: Intent?) {
         data?.let {
-            // TODO 5: Extract the text from result intent.
+            val receivedTxt : String? = data.getStringExtra(EXTRA_TEXT);
 
-
-            // TODO 6: Check if text is not null or empty, then set the new "chiuit" content.
-
-
-            TODO("13. Instantiate a new chiuit object that add it to the adapter")
+            if(!receivedTxt.isNullOrEmpty()){
+                listAdapter.addItem(Chiuit(receivedTxt))
+            }
         }
     }
 
